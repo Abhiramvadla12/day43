b0e179b6bf85dc21895735f78d2fa69e08d17b92
Abhiramvadla12 <abhiramvadla61@gmail.com>
2024-09-12 22:49:30 +0530

    version 3

--- index3.js
+++ index3.js
@@ -2,6 +2,9 @@ function isvowel (letter){
     if(letter === 'a' || letter === 'e'|| letter === 'i' || letter === 'o' || letter === 'u'){
         return `entered letter ${letter} is a vowel or True` 
     }
+    else if(letter === 'A' || letter === 'E'|| letter === 'I' || letter === 'O' || letter === 'U'){
+        return `entered letter ${letter} is a vowel or True`
+    }
     else{
         return `entered letter ${letter} is a consonant or false`
     }
@@ -9,4 +12,6 @@ function isvowel (letter){
 result = isvowel('a');
 console.log(result);
 result = isvowel('b');
+console.log(result);
+result = isvowel('A');
 console.log(result);
